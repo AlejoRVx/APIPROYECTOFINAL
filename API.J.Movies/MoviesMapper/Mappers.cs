8f16fa18db53d536810cafb14b5f59de20357f7f
AlejoRVx <alejandroramirezvela13@gmail.com>
2025-11-29 23:11:16 -0500

    Everything related to Category was added and the same was replicated for Movies

---
+++ API.J.Movies/MoviesMapper/Mappers.cs
@@ -0,0 +1,18 @@
+﻿using API.J.Movies.DAL.Dtos.Category;
+using API.J.Movies.DAL.Dtos.Movie;
+using API.J.Movies.DAL.Models;
+using AutoMapper;
+
+namespace API.J.Movies.MoviesMapper
+{
+    public class Mappers : Profile
+    {
+        public Mappers()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateUpdateDto>().ReverseMap();
+            CreateMap<Movie, MovieDto>().ReverseMap();
+            CreateMap<Movie, MovieCreateUpdateDto>().ReverseMap();
+        }
+    }
+}
---
+++ API.J.Movies/Repository/IRepository/IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.J.Movies.DAL.Models;
+
+namespace API.J.Movies.Repository.IRepository
+{
+    public interface IMovieRepository
+    {
+        Task<ICollection<Movie>> GetMoviesAsync(); //Me retorna una lista de peliculas
+        Task<Movie> GetMovieAsync(int id); //Me retorna una película en especifico
+        Task<bool> MovieExistsByIdAsync(int id); //Verifica si una película existe por id
+        Task<bool> MovieExistsByNameAsync(string name); //Verifica si una película existe por nombre
+        Task<bool> CreateMovieAsync(Movie movie); //Crea una nueva película
+        Task<bool> UpdateMovieAsync(Movie movie); //Actualiza una película
+        Task<bool> DeleteMovieAsync(int id); //Elimina una película buscando por id
+    }
+}
